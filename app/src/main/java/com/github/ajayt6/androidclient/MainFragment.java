0d4f6f06780fc5325bf95227bea130c15ec2fda5
Ajay Thomas <Ajay Thomas>
2016-10-17 02:42:31 -0600

    Code cleanup

--- app/src/main/java/com/github/ajayt6/androidclient/MainFragment.java
+++ app/src/main/java/com/github/ajayt6/androidclient/MainFragment.java
@@ -4,24 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
